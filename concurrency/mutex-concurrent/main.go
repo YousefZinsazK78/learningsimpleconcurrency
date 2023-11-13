0a03018654eab86cba9224b9a298a09e33edf1f3
Yousef ZinsazK78 <yousefzinsazk78@gmail.com>
2023-11-13 15:28:19 +0330

    add mutex

--- concurrency/mutex-concurrent/main.go
+++ concurrency/mutex-concurrent/main.go
@@ -1,1 +1,27 @@
-package mutex_concurrent
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	sum := 0
+
+	inc := func() {
+		sum += 10
+	}
+
+	dec := func() {
+		sum -= 10
+	}
+
+	for i := 0; i < 5; i++ {
+		go inc()
+	}
+
+	go dec()
+
+	time.Sleep(time.Second)
+	fmt.Println(sum)
+}
